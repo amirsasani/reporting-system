e4af7e044d502b4d1493a8c9fe6666e7ced5b912
Amir Sasani <sasani.amir.1998@gmail.com>
2022-07-14 16:35:31 +0430

    create service provider

---
+++ src/ReportingSystemServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Amirsasani\ReportingSystem;
+
+
+use Illuminate\Support\ServiceProvider;
+
+
+class ReportingSystemServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
