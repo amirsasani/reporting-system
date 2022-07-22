5ecfffd221c03bb11032b14a8e4bf59c1425d6d6
Amir Sasani <sasani.amir.1998@gmail.com>
2022-07-22 18:18:26 +0430

    publish migrations stub

--- src/ReportingSystemServiceProvider.php
+++ src/ReportingSystemServiceProvider.php
@@ -15,6 +15,11 @@ class ReportingSystemServiceProvider extends ServiceProvider
 
     public function boot()
     {
-
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_reports_table.php.stub' =>  database_path('migrations/' . date('Y_m_d_His', time()) . '_create_reports_table.php'),
+                __DIR__ . '/../database/migrations/create_report_logs_table.php.stub' =>  database_path('migrations/' . date('Y_m_d_His', time()) . '_create_report_logs_table.php'),
+            ], 'migrations');
+        }
     }
 }
