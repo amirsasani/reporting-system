8f1c8155f1239108f3cef6327a5ba568fa009d5e
Amir Sasani <sasani.amir.1998@gmail.com>
2022-07-26 15:02:43 +0430

    test report service class

--- tests/Unit/ReportsTest.php
+++ tests/Unit/ReportsTest.php
@@ -2,8 +2,11 @@
 
 namespace Amirsasani\ReportingSystem\Tests\Unit;
 
+use Amirsasani\ReportingSystem\Details\NicknameDetails;
 use Amirsasani\ReportingSystem\Models\Report;
+use Amirsasani\ReportingSystem\ReportSystem;
 use Amirsasani\ReportingSystem\Tests\TestCase;
+use Amirsasani\ReportingSystem\Tests\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ReportsTest extends TestCase
@@ -14,5 +17,21 @@ class ReportsTest extends TestCase
     {
         $report = Report::factory()->create(['user_type' => 'Fake\User']);
         $this->assertEquals('Fake\User', $report->user_type);
+    }
+
+    public function test_create_report_with_service()
+    {
+        $user = User::factory()->create();
+        $reportSystem = new ReportSystem($user);
+
+        $details = new NicknameDetails();
+        $details->setId(2);
+        $details->setDescription("user's nickname is against rules");
+        $details->setNickname("badusername");
+        $details->setSubject("nickname is not valid");
+
+        $newReport = $reportSystem->new('nickname', $details);
+
+        $this->assertDatabaseHas('reports', ['report_status' => Report::STATUS_PENDING]);
     }
 }
